49c3e21d5a0441bf42263421ab67112c7a13e3bf
heatherherrington <redherring@gmail.com>
2016-10-01 12:32:05 -0700

    Problem 3 and 4 solved

--- practice.rb
+++ practice.rb
@@ -1,16 +1,3 @@
-# For each of the following problems, you should write a single method to solve
-# the problem. You will be working with arrays in each problem, but should use
-# array indexing and loops to solve the problems. You should **not** utilize
-# any [Ruby Array methods](https://ruby-doc.org/core-2.2.0/Array.html), with
-# the exception of `<<` and `[]`.
-# e.g.
-# - `array.first` ==> NOT OK
-# - `puts array[0]` ==> OK
-# - `array.insert(i, array2[loc)` ==> NOT OK
-# - `array[i] = array2[loc]` ==> OK
-# - `array.pop` ==> NOT OK
-# - `array.uniq` ==> NOT OK ;) See problem 3 and 4.
-
 ## Problem #1
 def print_array(array_of_integers)
   print array_of_integers
@@ -44,13 +31,20 @@ def numUnique(integer_array)
 end
 
 ## Problem #4
-# Write a method named `numUnique2` that functions the same as `numUnique`
-# except that it **does NOT use a hash** to solve the problem. In this version,
-# you should only iterate over the array, but **use no auxiliary storage**
-# outside of fixnum variables, if needed.
-# Remember that you can assume that the values in the array appear in
-# sorted (nondecreasing) order.
-
-def numUnique2(sorted_integer_array)
-
+def numUnique2(input)
+  counter = 1
+  number = input[0]
+  if input.length == 0
+    counter = 0
+  end
+  input.length.times do |i|
+    if input[i] == number
+      i += 1
+    else
+      counter += 1
+      number = input[i]
+      i += 1
+    end
+  end
+  return counter
 end
