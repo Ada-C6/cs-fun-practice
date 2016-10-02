eaf9bc28fd2d341cd524d0f916b841a839cd180b
Briana Eng <brianaeng@gmail.com>
2016-10-02 14:05:52 -0700

    New solution(s) to problem #2

--- practice.rb
+++ practice.rb
@@ -1,15 +1,48 @@
 def stretch(list)
   new_list = []
 
-  list.each do |num|
-    if num % 2 == 0
-      new_num = num/2
-      new_list.push(new_num, new_num)
+  num = 0
+  until num == list.length
+    if list[num] % 2 == 0
+      new_list += [list[num]/2, list[num]/2]
     else
-      new_num = (num/2)
-      new_list.push(new_num + 1, new_num)
+      new_list += [list[num]/2 + 1, list[num]/2]
     end
+
+    num += 1
   end
 
   return new_list
 end
+
+#Initial solution - used .push & .each, going to get rid of those
+# def stretch(list)
+#   new_list = []
+#
+#   list.each do |num|
+#     if num % 2 == 0
+#       new_num = num/2
+#       new_list.push(new_num, new_num)
+#     else
+#       new_num = (num/2)
+#       new_list.push(new_num + 1, new_num)
+#     end
+#   end
+#
+#   return new_list
+# end
+
+#Next solution - got rid of .push and condensed it a bit, going to get rid of .each
+# def stretch(list)
+#   new_list = []
+#
+#   list.each do |num|
+#     if num % 2 == 0
+#       new_list += [num/2, num/2]
+#     else
+#       new_list += [num/2 + 1, num/2]
+#     end
+#   end
+#
+#   return new_list
+# end
