7eed07a789fda9c6d355fc1a52d7a55fd9086057
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-02 12:15:50 -0700

    solved problem #2, stretch

---
+++ stretch.rb
@@ -0,0 +1,48 @@
+
+# Without each: 
+
+def stretch(array)
+  new_arr = []
+  len = array.length
+  i = 0
+  count = 0
+
+  until count == len
+    if array[i] % 2 == 0
+      new_arr << array[i] / 2
+      new_arr << array[i] / 2
+    else
+      new_arr << (array[i] / 2) + 1
+      new_arr << array[i] / 2
+    end
+    count += 1
+    i +=1
+  end
+  return new_arr
+end
+
+
+# With each:
+
+# def stretch(array)
+#   new_arr = []
+#   len = array.length
+#   i = 0
+#   count = 0
+#
+#
+#   array.each do |num|
+#     half = num / 2
+#     if num % 2 == 0
+#       new_arr << half
+#       new_arr << half
+#     else
+#       new_arr << half + 1
+#       new_arr << half
+#     end
+#   end
+#   return new_arr
+# end
+
+# should return [9, 9, 4, 3, 2, 2, 12, 12, 6, 5]
+puts stretch([18, 7, 4, 24, 11])
