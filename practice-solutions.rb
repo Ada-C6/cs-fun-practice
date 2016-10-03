e2db97e566891e94bc5cdeeedc2eed08bd644987
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-03 14:56:37 -0700

    set up for github stuff with brackets lecture

--- practice-solutions.rb
+++ practice-solutions.rb
@@ -1,19 +1,38 @@
-def stretch(arr)
-  list = []
+# def stretch(arr)
+#   list = []
+#
+#   arr.each do |a|
+#     if a % 2 == 0
+#       2.times do
+#         list << a/2
+#       end
+#     else
+#       list << (a/2) + 1
+#       list << a/2
+#     end
+#
+#   end
+#   return list
+# end
+#
 
-  arr.each do |a|
-    if a % 2 == 0
-      2.times do
-        list << a/2
-      end
-    else
-      list << (a/2) + 1
-      list << a/2
-    end
 
-  end
-  return list
+#live code solution
+
+def stretch(input)
+  output = []
+  input.length.times do |i| #i is the index/counter at 0, so must do input[i] to test the value at the index
+  split_value = input[i] / 2
+
+  #ternary operator
+  output[2 * i] = (input[i] % 2 == 0 ? split_value : split_value + 1)
+  # if input[i] % 2 == 0
+  #   output << split_value
+  # else
+  #   split_value = input[i] / 2
+  #   output << split_value + 1
+  # end
+    output[2 * i + 1] = split_value
 end
-
-
-  
+  return output
+end
