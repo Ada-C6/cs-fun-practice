f1642c4deba81b3f8c8705443b7370551d46ffb4
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-02 22:20:59 -0700

    Did fourth problem and cleaned up third problem. Also practicing git branching

--- problem_3.rb
+++ problem_3.rb
@@ -1,4 +1,4 @@
-# This method takes a SORTED array of integers as input and returns the number of unique walues in input array, using a hash.
+# This method takes a SORTED array of integers as input and returns the number of unique values in input array, using a hash.
 
 def numUnique(array)
   h = {}
@@ -8,5 +8,3 @@ def numUnique(array)
   end
   return h.length
 end
-
-puts numUnique [5, 7, 7, 7, 8, 22, 22, 23, 31, 35, 35, 40, 40, 40, 41]
