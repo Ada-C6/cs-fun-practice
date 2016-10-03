cc288fe832ee35fb529995e589130b054375ee2b
Mary Morrison <marymorrison15@gmail.com>
2016-10-02 21:58:13 -0700

    problem 2 complete

--- solution_cs-fun-prac.rb
+++ solution_cs-fun-prac.rb
@@ -3,3 +3,26 @@
 def print_array(input)
   print input
 end
+
+
+## Problem #2
+
+def stretch(array)
+  stretched_array = []
+  element = 0
+  until element == array.length
+    num = array[element]
+    if num % 2 == 0
+      2.times do
+        stretched_array << num/2
+      end
+    else
+      odd = num/2
+      even = odd + 1
+      stretched_array << even
+      stretched_array << odd
+    end
+    element += 1
+  end
+  return stretched_array
+end
