bf4264305c4d2ac92def98b7b13ffc6b916b8cd0
Jessica <jchung722@gmail.com>
2016-10-04 09:20:20 -0700

    refactored method

--- stretch.rb
+++ stretch.rb
@@ -1,16 +1,8 @@
 def stretch(line)
   if line.length == 1
-    if line[0] % 2 != 0
-      return [line[0]/2 + 1, line[0]/2]
-    else
-      return [line[0]/2, line[0]/2]
-    end
+      return [line[0]/2 + line[0] % 2, line[0]/2]
   else
-    if line[0] % 2 != 0
-      return [line[0]/2 + 1, line[0]/2] + stretch(line[1..line.length])
-    else
-      return [line[0]/2, line[0]/2] + stretch(line[1..line.length])
-    end
+      return [line[0]/2 + line[0] % 2, line[0]/2] + stretch(line[1..line.length])
   end
 end
 
