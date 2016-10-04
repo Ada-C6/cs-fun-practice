90840bc02be601bf114731720e0c0c90cdcdf24a
Nina Mutty <ninamutty@gmail.com>
2016-10-04 09:20:01 -0700

    added cleaner method

--- problem_2.rb
+++ problem_2.rb
@@ -6,19 +6,31 @@
 # For example, if a variable named list refers to an array storing the values [18, 7, 4, 24, 11], the call of stretch(list) should return a new array containing [9, 9, 4, 3, 2, 2, 12, 12, 6, 5]. (The number 18 is stretched into the pair 9, 9, the number 7 is stretched into 4, 3, the number 4 is stretched into 2, 2, the number 24 is stretched into 12, 12 and the number 11 is stretched into 6, 5.)
 
 
+# def stretch(array)
+#   new_array = []
+#   array.each do |number|
+#     if number % 2 == 0
+#       2.times do
+#         new_array << (number/2)
+#       end
+#     else
+#       new_array << (((number-1)/2) + 1)
+#       new_array << ((number-1)/2)
+#     end
+#   end
+#   return new_array
+# end
+
+##################
+### Better way ###
+##################
 def stretch(array)
   new_array = []
   array.each do |number|
-    if number % 2 == 0
-      2.times do
-        new_array << (number/2)
-      end
-    else
-      new_array << (((number-1)/2) + 1)
-      new_array << ((number-1)/2)
-    end
+    new_array << (number/2) + (number % 2)
+    new_array << (number/2)
   end
-  return new_array
+  new_array
 end
 
 
@@ -28,12 +40,11 @@ end
 
 
 
-
-
-
 raise "strech broke - stretch([18, 7, 4, 24, 11])" unless stretch([18, 7, 4, 24, 11]) == [9, 9, 4, 3, 2, 2, 12, 12, 6, 5]
 raise "strech broke - strech({[4, 5, 3]})" unless stretch([4, 5, 3]) == [2, 2, 3, 2, 2, 1]
 raise "strech broke - strech({[]})" unless stretch([]) == []
+raise "strech broke - strech({[0]})" unless stretch([0]) == [0, 0]
+
 puts "passes all strech tests"
 
 puts "All tests passed"
