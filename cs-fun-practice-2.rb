79b3abad1cbbafd4f32aa119e50f0b321151960f
Maya Wang <salome.neptune@gmail.com>
2016-10-14 23:47:57 -0700

    completed practice 2 problem 2

--- cs-fun-practice-2.rb
+++ cs-fun-practice-2.rb
@@ -10,27 +10,33 @@ def mirror(array)
   return array + temp
 end
 
+puts "\n=====TEST:NORMAL ARRAY========"
 array = [1, 3, 2, 7]
 print mirror(array)
 #=> [1, 3, 2, 7, 7, 2, 3, 1]
+puts "\n=====TEST:EMPTY ARRAY========"
 array = []
 print mirror(array)
 # => []
 
 ## Problem #2
 def switchPairs(arr_str)
-  for i in 0...arr_str.length
-    if arr_str.length % 2 == 0
-      
-    end
+  temp = []
+  for i in 0...arr_str.length/2
+    temp << arr_str[2 * i + 1]
+    temp << arr_str[2 * i]
   end
+  if arr_str.length % 2 == 1
+    temp << arr_str[-1]
+  end
+  return temp
 end
 
-puts "\n============="
+puts "\n=====TEST:ODD SIZE ARRAY========"
 arr_str = ["to", "be", "or", "not", "to", "be", "hamlet"]
-puts switchPairs(arr_str)
+print switchPairs(arr_str)
 #=> ["be", "to", "not", "or", "be", "to", "hamlet"]
-puts "\n============="
+puts "\n=====TEST:EVEN SIZE ARRAY========"
 arr_str = ["to", "be", "or", "not", "to", "be", "hamlet","to"]
-puts switchPairs(arr_str)
-#=> ["be", "to", "not", "or", "be", "to", "hamlet", "to"]
+print switchPairs(arr_str)
+#=> ["be", "to", "not", "or", "be", "to", "to", "hamlet"]
