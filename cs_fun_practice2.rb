32aa98318e7db72605055eff09d50e79fe1e23ee
Trish Griego <pmgriego@gmail.com>
2016-10-16 16:21:27 -0700

    Finished working with problem #1. Moving on to #2

--- cs_fun_practice2.rb
+++ cs_fun_practice2.rb
@@ -7,25 +7,33 @@
 # [ orginal    | + orginal reversed]
 # [1, 2, 3, 4, 4, 4, 3, 2, 1]
 
-# I want to take the index values of one array
-      #[0,1,2,3]
-list = [1,2,3,4]
+# This will still work with whatever array list is given. I choose to assume a small array for the purpose of checking.
+# I noticed others added a validation for making sure arguments/params provided weren't empty.
+
+list = [1,2,3,6]
 
 def mirror(list)
-  if list ! nil
+  # create a variable in which to store value
     new_array = []
+  # num will now store the total length of the given array
+  # In this iteration: 4
     num = list.length
-
+    # loop through given array for the indexed length.
     num.times do |i|
+      # store those values at new_array.
+      # [1,2,3,6]
       new_array << list[i]
       i += 1
     end
     num.times do |i|
+      # loop through array again but start from end of index
       i += 1
+      # store them with the previously stored data.
       new_array << list[-i]
     end
     return new_array
-  end
 end
 
-puts "The orginal array = #{list}. However, when we call method mirror on list. mirror(list) it returns #{mirror(list)}"
+puts "The orginal array = #{list}. However, when we call the method mirror on list: mirror(list) it returns #{mirror(list)}"
+
+# Predict mirror(list) will return => [][1, 2, 3, 6, 6, 3, 2, 1]
