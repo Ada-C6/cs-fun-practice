e89264c146e62af28b597de3395e228f18c78b0f
Sarah Nahmias <nahmisa@gmail.com>
2016-10-16 18:19:22 -0700

    Solves switchPairs

--- practice_two.rb
+++ practice_two.rb
@@ -19,3 +19,30 @@ def mirror(list)
   list
 
 end
+
+
+def switchPairs(list)
+# index over the array but incement by two since it is pairs
+# switch must happen in place
+# do not switch if at the end of the array (aka the array is odd) -- the last "pairee"'s value will be nil
+
+  index = 0
+
+  until index == list.length do
+    item1 = list[index]
+    item2 = list[index + 1]
+
+    unless item2 == nil
+
+      list[index] = item2
+      list[index + 1] = item1
+
+      index += 2
+    end
+# need to return here to break the loop of odd case -
+# in that case we skip the step of adding to index
+# length will never be equal to index.
+# Does not affect even case.
+    return list
+  end
+end
